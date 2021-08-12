f75357dd4ab85355ddb8a62214126f4c05e53c77
dsorensen-mck <ext-d.sorensen@rappi.com>
2021-08-12 18:55:49 -0300

    second commit with rest of scenario example

--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -2,24 +2,77 @@ package io.cucumber.skeleton;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.es.*;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
 
 public class StepDefinitions {
-    
+
+    private WebDriver driver;
 
 
-    @Dado("estoy  carroYa website")
-    public void I_go_to() {
+    @Dado("^que estoy en website de (.*)$")
+    public void I_go_to(String url) {
+        System.setProperty("webdriver.chrome.driver","/Users/diego_sorensen/dev/drivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        driver.navigate().to(url);
+       // driver.close();
+
+    }
+
+    @Dado("que estoy en la pagina de carroya")
+    public void que_estoy_en_la_pagina_de_carroya() {
+        // Write code here that turns the phrase above into concrete actions
+        driver.findElement(By.className("placeholderText")).click();
+
+    }
+
+    @Cuando("^busco un carro de (.*) con (.*)$")
+    public void busco_un_carro_parameter(String marca, String linea) throws InterruptedException {
+        System.out.println(marca);
+        System.out.println(linea);
+
+        driver.findElement(By.className("placeholderText")).click();
+
+
+
+        driver.findElement(By.xpath(String.format("//h3[(.='%s')]",marca))).click();
+
+        Thread.sleep(3000);
+
+        driver.findElement(By.xpath("//*[contains(text(),'Todas las líneas')]")).click();
+
+        //
+        //WebDriverWait wait = new WebDriverWait(WebDriverRefrence,TimeOut);
+
+        driver.findElement(By.xpath(String.format("//h3[(.='%s')]",linea))).click();
+
+
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//button/h3[contains(text(),'Buscar')]")).click();
+
+
+    }
+
+
+    @Entonces("quiero verificar que la busqueda retorne vehiculos")
+    public void verificar_vehiculos() throws InterruptedException {
+        // Write code here that turns the phrase above into concrete actions
         System.out.println("");
-
+        Thread.sleep(2000);
+        String offers = driver.findElement(By.className("titleTotalVehicles")).getText();
+        short numberofCars=0;
+        offers = offers.replaceAll("\\D+","");
+        numberofCars = Short.parseShort(offers);
+        boolean boolFindCars = numberofCars >0 ;
+        Assert.assertTrue("Numero de vehiculos menor a cero",boolFindCars);
+        driver.close();
     }
 
-
-    @Cuando("busco un carro de <marca> con <linea>")
-    public void buscoUnCarroDeMarcaConLinea() {
-        
-    }
-
-    @Entonces("quiero obtener la cantidad de resultados")
-    public void quieroObtenerLaCantidadDeResultados() {
-    }
 }
--- src/test/resources/io/cucumber/skeleton/carroya.feature
+++ src/test/resources/io/cucumber/skeleton/carroya.feature
@@ -2,10 +2,12 @@
  Característica: Busqueda de un carro
 
   Esquema del escenario: Busqueda de un carro en el portal carroya
-     Dado que estoy en la pagina de carroya
-     Cuando busco un carro de <marca> con <linea>
-     Entonces quiero obtener la cantidad de resultados
-    Ejemplos:
-      |  marca|    linea|
-      |Chevrolet|  Alto |
 
+     Dado que estoy en website de http://www.carroya.com
+     Cuando busco un carro de <marca> con <linea>
+     Entonces quiero verificar que la busqueda retorne vehiculos
+    Ejemplos:
+      |  marca |    linea|
+      |Chevrolet|  Alto |
+      |Ford     |  Aerostar |
+      |Aro     |  Carpati |
